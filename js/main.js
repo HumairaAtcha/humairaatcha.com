769e0dca5c24d4728ea53704bba655be8d5250d1
Humaira Atcha <humaira.atcha@autotrader.co.uk>
2018-04-25 15:13:46 +0100

    new theme, text all done, things need moving around, maps doesn't work, no portfolio

--- js/main.js
+++ js/main.js
@@ -1,114 +1,26 @@
-$(document).ready(function() {
-
-
-    /* Scroll hire me button to contact page */
-    $('.hire-me').click(function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500);
-        return false;
-    });
-
-    /* For Bootstrap current state on portfolio sorting */
-
-    $('ul.nav-pills li a').click(function(e) {
-        $('ul.nav-pills li.active').removeClass('active')
-        $(this).parent('li').addClass('active')
-    })
-
-    /* portfolio mixitup */
-
-    $(window).load(function() {
-        var $container = $('.grid-wrapper');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
+(function e(t, n, r) {
+    function s(o, u) {
+        if (!n[o]) {
+            if (!t[o]) {
+                var a = typeof require == "function" && require;
+                if (!u && a) return a(o, !0);
+                if (i) return i(o, !0);
+                throw new Error("Cannot find module '" + o + "'")
             }
-        });
-
-        $('.grid-controls li a').click(function() {
-            $('.grid-controls .current').removeClass('current');
-            $(this).addClass('current');
-
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-    });
-
-
-    /* Magnific Popup */
-    $('.grid-wrapper').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: {
-            enabled: true
+            var f = n[o] = {exports: {}};
+            t[o][0].call(f.exports, function (e) {
+                var n = t[o][1][e];
+                return s(n ? n : e)
+            }, f, f.exports, e, t, n, r)
         }
-    });
+        return n[o].exports
+    }
 
+    var i = typeof require == "function" && require;
+    for (var o = 0; o < r.length; o++) s(r[o]);
+    return s
+})({
+    1: [function (require, module, exports) {
 
-    /* Sticky menu */
-    $(".navbar").sticky({
-        topSpacing: 0
-    });
-
-
-    /* Scroll spy and scroll filter */
-    $('#main-menu').onePageNav({
-        currentClass: "active",
-        changeHash: false,
-        scrollThreshold: 0.5,
-        scrollSpeed: 750,
-        filter: "",
-        easing: "swing"
-    });
-
-    /* Charts*/
-
-    $('.chart').waypoint(function() {
-        $(this).easyPieChart({
-            barColor: '#f1c40f',
-            size: '150',
-            easing: 'easeOutBounce',
-            onStep: function(from, to, percent) {
-                $(this.el).find('.percent').text(Math.round(percent));
-            }
-        });
-    }, {
-        triggerOnce: true,
-        offset: 'bottom-in-view'
-    });
-
-
-    /* VEGAS Home Slider */
-	
-	    $('#page-welcome').vegas({
-        slides: [
-            { src: 'img/slider/01.jpg' },
-            { src: 'img/slider/02.jpg' },
-           /* { src: 'img/slider/03.jpg' },
-			{ src: 'img/slider/04.jpg' }*/
-        ],
-    overlay: true
-});
-
-    $("#vegas-next").click(function() {
-        $('#page-welcome').vegas('next');
-    });
-    $("#vegas-prev").click(function() {
-       $('#page-welcome').vegas('previous');
-    });
-
-
-
-});
+    }, {}]
+}, {}, [1])
